a17d74acc846a3e4d9199d7de12cfe1b4f504216
Eliott Wantz <70651737+EliottWantz@users.noreply.github.com>
2023-02-09 12:01:13 -0500

    ziglearn.org Chapter1

--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,31 @@
 const std = @import("std");
+const expect = @import("std").testing.expect;
 
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
 
+    const elems = [_]u8{ 3, 4, 5 };
+    const len = elems.len;
+    std.debug.print("{d}\n", .{elems});
+    std.log.info("Elems len = {d}\n", .{len});
+
+    const string = [_]u8{ 'a', 'b', 'c' };
+
+    for (string) |char, index| {
+        std.log.info("Index = {d}, Char = {d}\n", .{ index, char });
+    }
+
+    @setRuntimeSafety(false);
+    const a = [3]u8{ 1, 2, 3 };
+    var index: u8 = 5;
+    const b = a[index];
+    std.log.info("What is the value of b? {d}\n", .{b}); // It takes the last elem in array
+
     // stdout is for the actual output of your application, for example if you
     // are implementing gzip, then only the compressed bytes should be sent to
     // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
+    var bw = std.io.bufferedWriter(std.io.getStdOut().writer());
     const stdout = bw.writer();
 
     try stdout.print("Run `zig build test` to run the tests.\n", .{});
@@ -21,4 +38,194 @@ test "simple test" {
     defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
     try list.append(42);
     try std.testing.expectEqual(@as(i32, 42), list.pop());
+}
+
+test "if statement" {
+    const a = true;
+    var x: u8 = 0;
+    if (a) {
+        x += 1;
+    } else {
+        x += 2;
+    }
+    try expect(x == 1);
+}
+
+test "if statement expression" {
+    const a = true;
+    var x: u8 = 0;
+    x += if (a) 1 else 2;
+    try expect(x == 1);
+}
+
+test "while" {
+    var i: u8 = 2;
+    while (i < 100) {
+        i += 2;
+    }
+    try expect(i == 100);
+}
+
+test "while with continue expression" {
+    var sum: u8 = 0;
+    var i: u8 = 1;
+    while (i <= 10) : (i += 1) {
+        sum += 1;
+    }
+    try expect(i == 11);
+}
+
+test "while with continue" {
+    var sum: u8 = 0;
+    var i: u8 = 0;
+    while (i <= 3) : (i += 1) {
+        if (i == 2) continue;
+        sum += i;
+    }
+    try expect(sum == 4);
+}
+
+test "while with break" {
+    var sum: u8 = 0;
+    var i: u8 = 0;
+    while (i <= 3) : (i += 1) {
+        if (i == 2) break;
+        sum += i;
+    }
+    try expect(sum == 1);
+}
+
+test "for" {
+    const string = [_]u8{ 'a', 'b', 'c' };
+
+    for (string) |char, index| {
+        std.log.info("Index = {d}, Char = {d}\n", .{ index, char });
+    }
+
+    for (string) |char| {
+        _ = char;
+    }
+
+    for (string) |_, index| {
+        _ = index;
+    }
+
+    for (string) |_| {}
+}
+
+test "out of bounds" {
+    @setRuntimeSafety(false);
+    const a = [3]u8{ 1, 2, 3 };
+    var index: u8 = 5;
+    const b = a[index];
+    _ = b;
+}
+
+fn asciiToUpper(x: u8) u8 {
+    return switch (x) {
+        'a'...'z' => x + 'A' - 'a',
+        'A'...'Z' => x,
+        else => unreachable,
+    };
+}
+
+test "unreachable switch" {
+    try expect(asciiToUpper('a') == 'A');
+    try expect(asciiToUpper('A') == 'A');
+    // try expect(asciiToUpper('[') == 'A');
+}
+
+fn increment(num: *u8) void {
+    num.* += 1;
+}
+
+test "pointers" {
+    var x: u8 = 1;
+    increment(&x);
+    try expect(x == 2);
+}
+
+// test "naughty pointer" {
+//     var x: u16 = 0;
+//     var y: *u8 = @intToPtr(*u8, x);
+//     _ = y;
+// }
+
+// test "const pointers" {
+//     const x: u8 = 1;
+//     var y = &x;
+//     y.* += 1;
+// }
+
+// Slices
+fn total(values: []const u8) usize {
+    var sum: usize = 0;
+    for (values) |v| sum += v;
+    return sum;
+}
+
+test "slices" {
+    const array = [_]u8{ 1, 2, 3, 4, 5 };
+    const slice = array[0..3];
+    try expect(total(slice) == 6);
+}
+
+test "slices 2" {
+    const array = [_]u8{ 1, 2, 3, 4, 5 };
+    const slice = array[0..3];
+    try expect(@TypeOf(slice) == *const [3]u8);
+}
+
+test "slices 3" {
+    var array = [_]u8{ 1, 2, 3, 4, 5 };
+    var slice = array[0..];
+    try expect(total(slice) == 15);
+    var slice2 = slice[3..];
+    try expect(total(slice2) == 9);
+}
+
+// Enums
+const Direction = enum { north, south, east, west };
+const Value = enum(u2) { zero, one, two };
+
+test "enum ordinal value" {
+    try expect(@enumToInt(Value.zero) == 0);
+    try expect(@enumToInt(Value.one) == 1);
+    try expect(@enumToInt(Value.two) == 2);
+}
+
+const Value2 = enum(u32) {
+    hundred = 100,
+    thousand = 1000,
+    million = 1000000,
+    next,
+};
+
+test "set enum ordinal value" {
+    try expect(@enumToInt(Value2.hundred) == 100);
+    try expect(@enumToInt(Value2.thousand) == 1000);
+    try expect(@enumToInt(Value2.million) == 1000000);
+    try expect(@enumToInt(Value2.next) == 1000001);
+}
+
+const Suit = enum {
+    clubs,
+    spades,
+    diamonds,
+    hearts,
+
+    pub fn isClubs(s: Suit) bool {
+        return s == Suit.clubs;
+    }
+
+    pub fn isHearts(self: Suit) bool {
+        return self == Suit.hearts;
+    }
+};
+
+test "emum method" {
+    try expect(Suit.spades.isClubs() == Suit.isClubs(.spades));
+    const mySuit: Suit = Suit.hearts;
+    try expect(mySuit.isHearts());
+    try expect(Suit.isHearts(Suit.clubs) == Suit.isClubs(.hearts));
 }
